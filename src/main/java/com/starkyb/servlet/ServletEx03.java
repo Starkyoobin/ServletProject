b0324fd606b7679629fecd327eb9426ffe911225
DESKTOP-8NADJPO\yb010 <yb010626@gmail.com>
2021-07-22 15:47:01 +0900

    [김유빈] -get method 일부 예제

---
+++ src/main/java/com/starkyb/servlet/ServletEx03.java
@@ -0,0 +1,27 @@
+package com.starkyb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//http://localhost:8080/servlet/ex03?user_id=starkyb&name=kim?age=30
+@WebServlet("/servlet/ex03")
+public class ServletEx03 extends HttpServlet{
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String id = request.getParameter("user_id");
+		String name = request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		
+		out.println("<html><head><title>get 파마미터 확인</title></head>");
+		out.println("<body><h3>" + id + " : " + name + " - " + age + "</h3></body></html>");
+	}
+}
